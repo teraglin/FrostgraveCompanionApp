fa964bea289afcfce84cd56782ca0e9fed2ad5b2
Aidan Kirvan <apkirvan@hotmail.com>
2023-05-23 22:02:26 +1000

    Task: status rendering functions

--- App.js
+++ App.js
@@ -5,17 +5,47 @@ import * as characterData from "./utils/characterData.json"
 export default function App() {
   // const characterData = require('./utils/characterData.json')
   const character = characterData.character
-  const apprentice = character.apprentice
-  console.log(character)
+  const apprentice = characterData.apprentice
+  const warband = characterData.warband
+  const unitCount = warband.length
+  const unitSpace = 10 - warband.length
 
-  function determinePrefix(stat) {
+  console.log(unitCount)
+
+  function determinePrefix(object, stat) {
     if (stat === "fight" || stat === "shoot" || stat === "will") {
-      if (character.stats[stat] >= 0) {
+      if (object.stats[stat] >= 0) {
         return "+"
       }
       return ""
     }
     return ""
+  }
+
+  function renderStats(object) {
+    return Object.keys(object.stats).map((key, index) => {
+      return (
+        <Text key={index}>{key}: {determinePrefix(object, key)}{object.stats[key]}</Text>
+      )
+    })
+  }
+
+  const renderWarband = () => {
+    for (i = unitCount; i < unitCount; i++) {
+      console.log(warband[i])
+      return (
+        <>
+          <Text>Name: {warband[i].name}</Text>
+          <Text>Class: {warband[i].class}</Text>
+          {renderStats(warband[i])}
+        </>
+      )
+    }
+  }
+  const renderEmptyWarband = () => {
+    for (i = unitCount; i < unitSpace; i++) {
+      return <Text>...</Text>
+    }
   }
 
   return (
@@ -27,21 +57,21 @@ export default function App() {
         </Text>
       </View>
       <View style={styles.content}>
-        <Text>Wizard</Text>
+        <Text style={styles.sectionHeading}>Wizard</Text>
         <Text style={styles.description}>Name: {character.name}</Text>
         <Text style={styles.description}>School: {character.school}</Text>
         <Text style={styles.description}>Gold: {character.gold}</Text>
-        {Object.keys(character.stats).map((key, index) => {
-          return (
-            <Text key={index}>{key}: {determinePrefix(key)}{character.stats[key]}</Text>
-          )
-        })}
+        {renderStats(character)}
         {character.apprentice && (
           <>
-            <Text>Apprentice</Text>
+            <Text style={styles.sectionHeading}>Apprentice</Text>
             <Text>Name: {apprentice.name}</Text>
+            {renderStats(apprentice)}
           </>
         )}
+        <Text style={styles.sectionHeading}>Warband</Text>
+        {renderWarband()}
+        {renderEmptyWarband()}
       </View>
     </View>
   );
@@ -82,9 +112,13 @@ const styles = StyleSheet.create({
   },
   description: {
     textAlign: "left",
-    alignItems: "start",
-    justifyContent: "start",
+    // alignItems: "start",
+    // justifyContent: "start",
     // borderWidth: 1
+  },
+  sectionHeading: {
+    width: "100%",
+    textAlign: "center"
   }
 
 });
